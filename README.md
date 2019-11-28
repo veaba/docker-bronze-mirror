5dbc92452663a6990eff8a84a3743023fbef0503
Godpu <908662421@qq.com>
2019-11-28 22:29:20 +0800

    update test

--- README.md
+++ README.md
@@ -10,16 +10,21 @@
 宕机、容器启动等通过websocket 推送消息
 
 
-- actions
-- containers
-- images
-- app
-- docker
-- screens
-- status
-- websocket
-- healthy
-- host 宿主
+- actions       动作
+- containers    容器
+- images        镜像
+- app           应用
+- docker        docker
+- screens       大屏
+- status        状态
+- websocket     websocket
+- healthy       心跳
+- host 宿主     宿主机
+- notify        通知
+- webhooks      webhooks
+- im            即时消息
+- channel       通道
+- utils         工具类库
 # python 版本 3.7.2 32-bit 
 
 ## 如果出现 
--- utils.py
+++ utils.py
@@ -1,1 +1,29 @@
-get_name_by_
+import  re 
+import  os 
+IP_MAPS={
+    "127.0.0.1":"张三",
+    "192.168.1.1":"李四"
+}
+# 根据ip获取用户名称
+def get_name_by_ip(ip):
+    return IP_MAPS[ip] or "未知"
+
+def host_who():
+    who=os.popen('who')
+    std_lines= who.readlines()
+    time_ip_map={}
+    for line in std_lines:
+        time_and_ip=re.sub(r'^.*        ','',line.strip())
+        time_and_ip=time_and_ip.split(' (')
+        time_ip_map[time_and_ip[0]]=time_and_ip[1][0:-1]
+        print('---------')
+        print(line)
+        print('=========')
+    print(time_ip_map)
+
+# 2019-11-28 22:11 转时间戳 => 1574950260
+def time_to_timestmap():
+    pass
+
+if  __name__ == "__main__":
+    host_who()
